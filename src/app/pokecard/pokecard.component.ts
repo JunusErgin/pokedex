0f00e3558e826e7939636fe20e49c4794cda6bb8
Junus Ergin <junus91@googlemail.com>
2021-11-02 12:55:11 +0100

    hero tag

--- src/app/pokecard/pokecard.component.ts
+++ src/app/pokecard/pokecard.component.ts
@@ -17,44 +17,68 @@ export class PokecardComponent implements OnInit, AfterViewInit {
     name: 'Loading',
     url: 'https://pokeapi.co/api/v2/pokemon/1/',
   };
+  pokemonIsOpen = false;
   pokemonInfo: any = {};
   bgColor = '#94C9AD';
   @ViewChild('pokemonCard') pokemonCard: ElementRef;
   @ViewChild('pokemonCardCopy') pokemonCardCopy: ElementRef;
   position = { left: 0, top: 0 };
-  constructor() {}
+  constructor() { }
 
   async ngOnInit() {
     // console.log('Pokemon is', this.pokemon);
     this.pokemon.name = capitalizeFirstLetter(this.pokemon.name);
     let resp = await fetch(this.pokemon.url);
     this.pokemonInfo = await resp.json();
-    // console.log(this.pokemonInfo);
+    console.log(this.pokemonInfo);
     let type = this.pokemonInfo.types[0]['type']['name'];
     // console.log(type);
     this.bgColor = this.getColorByType(type);
-    console.log(
-      '-----',
-      this.pokemonCard.nativeElement.getBoundingClientRect()
-    );
+
     this.position = this.pokemonCard.nativeElement.getBoundingClientRect();
     this.pokemonCardCopy.nativeElement.style = `left: ${this.position.left - 12}px; top: ${this.position.top - 12}px;`;
   }
 
-  ngAfterViewInit() {}
+  ngAfterViewInit() { }
 
-  open(){
-    console.log('open');
+
+  getFirstType() {
+    let type = '';
+    try {
+      type = this.pokemonInfo['types'][0].type['name']
+    } catch (e) {
+
+    }
+
+    return type;
+  }
+  open() {
+    //console.log('open');
     this.pokemonCardCopy.nativeElement.style.display = 'flex';
     this.pokemonCardCopy.nativeElement.style.backgroundColor = this.bgColor;
-    setTimeout( () => {
-      this.pokemonCardCopy.nativeElement.style.width = '70%';
+    this.pokemonIsOpen = true;
+    setTimeout(() => {
+      this.pokemonCardCopy.nativeElement.style.width = '35%';
       this.pokemonCardCopy.nativeElement.style.top = '24px';
-      this.pokemonCardCopy.nativeElement.style.left = '15%';
+      this.pokemonCardCopy.nativeElement.style.left = '50%';
       this.pokemonCardCopy.nativeElement.style.right = '15%';
       this.pokemonCardCopy.nativeElement.style.height = '90vh';
     }, 10);
-    
+
+  }
+
+  closeCard() {
+    this.pokemonIsOpen = false;
+    //console.log('Closing', this.position);
+    this.pokemonCardCopy.nativeElement.style.width = this.position['width'] - 12 + 'px';
+    this.pokemonCardCopy.nativeElement.style.top = this.position.top - 12 + 'px';
+    this.pokemonCardCopy.nativeElement.style.left = this.position.left - 12 + 'px';
+    this.pokemonCardCopy.nativeElement.style.right = this.position['right'] - 12 + 'px';
+    this.pokemonCardCopy.nativeElement.style.height = this.position['height'] - 12 + 'px';
+
+    setTimeout(() => {
+      this.pokemonCardCopy.nativeElement.style.display = 'none';
+    }, 5000);
   }
 
   getColorByType(type) {
